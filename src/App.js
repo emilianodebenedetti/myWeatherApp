e070d847c490c657a511b857e686401d2ce75c80
Emiliano <emilianodebenedetti@outlook.com>
2024-01-03 12:39:32 -0300

    Agrego imagen de fondo y colores en fuentes y cards

--- src/App.js
+++ src/App.js
@@ -25,59 +25,58 @@ const App = () => {
 
   return (
     <>
-      <div className="hero min-h-screen bg-base-200">
+      <div className="bg-[url('https://c.wallhere.com/photos/a4/5e/nature_landscape_sky_field_Sun_clouds-225921.jpg!d')] hero min-h-screen ">
         <div className="hero-content flex-col ">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Pronóstico del tiempo</h1>
-            <p className="py-6">detalle pronóstico</p>
+          <div className="text-center">
+            <h1 className="text-5xl font-black text-blue-900">Pronóstico del tiempo</h1>
+            {/* <p className="py-6">detalle pronóstico</p> */}
           </div>
-          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl backdrop-blur-md">
             <div className="card-body">
-          
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Elige un país</span>
-              </label>
-              <select onChange={countryHandler} className='input input-bordered'>
-                  <option value="">Seleccionar</option>
-                  {countries
-                    .filter(country => country && country.name && country.name.common)
-                    .sort((a, b) => a.name.common.localeCompare(b.name.common))
-                    .map((country) => (
-                      <option key={country.cca2} value={country.cca2}>
-                        {country.name.common}
+              <div className="form-control ">
+                <label className="label">
+                  <span className="label-text font-black text-blue-900">Elige un país</span>
+                </label>
+                <select onChange={countryHandler} className='input input-bordered'>
+                    <option className="bg-color-blur-sm" value="">Seleccionar</option>
+                    {countries
+                      .filter(country => country && country.name && country.name.common)
+                      .sort((a, b) => a.name.common.localeCompare(b.name.common))
+                      .map((country) => (
+                        <option className="backdrop-blur-sm" key={country.cca2} value={country.cca2}>
+                          {country.name.common}
+                        </option>
+                    ))}
+                </select>
+              </div>
+              {cities.length > 0 && (
+                <div className="form-control">
+                  <label className="label" >
+                    <span className="label-text font-black text-blue-900">Ahora, elige una ciudad...</span>
+                  </label>
+                  <select onChange={cityHandler} className="input input-bordered">
+                    <option classNam="backdrop-blur-sm" value="">Seleccionar</option>
+                    {cities
+                    .filter(city => city && city.name)
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((city) => (
+                      <option key={city.id}> 
+                        {city.name} 
                       </option>
-                  ))}
-              </select>
-            </div>
-          {cities.length > 0 && (
-            <div className="form-control">
-              <label className="label" >
-                <span className="label-text">Ahora, elige una ciudad...</span>
-              </label>
-                <select onChange={cityHandler} className="input input-bordered">
-                <option value="">Seleccionar</option>
-                {cities
-                .filter(city => city && city.name)
-                .sort((a, b) => a.name.localeCompare(b.name))
-                .map((city) => (
-                  <option key={city.id}> 
-                    {city.name} 
-                  </option>
-                  ))}
-              </select>
-            </div>
-          )}
+                    ))}
+                  </select>
+                </div>
+              )}
             </div>
           </div>
           { weather && (
-            <div className="card w-96 h-56 bg-base-100 shadow-xl image-full"> 
+            <div className="card w-96 h-56 shadow-xl font-bold backdrop-blur-lg"> 
                 <figure><img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt='weather icon'/></figure>
-                <div className="card-body">
+                <div className="card-body text-blue-900">
                   <p>{weather.name}</p>
                   <div>
-                    <h2 className=" text-right">Temperatura actual: {weather.main.temp.toFixed()}ºC</h2>
-                    <h2 className=" text-right">Sensación térmica: {weather.main.feels_like.toFixed()}ºC</h2>
+                    <h2 className="text-right">Temperatura actual: {weather.main.temp.toFixed()}ºC</h2>
+                    <h2 className="text-right">Sensación térmica: {weather.main.feels_like.toFixed()}ºC</h2>
                   </div>
                 </div>
             </div>
